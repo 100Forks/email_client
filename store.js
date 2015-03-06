b70c863e874be0b5635be902836b4199234f415a
Duane M. Moody <dmmoody@gmail.com>
2015-03-06 10:23:42 -0800

    add test data to start framing the app

---
+++ store.js
@@ -0,0 +1,30 @@
+var inbox = [
+  {
+    sender: 'Great Dane Club',
+    receiver: 'dmmoody@gmail.com',
+    subject: 'This weekends gathering info',
+    date: 'Feb 3, 2010',
+    body: 'Hey, we are meeting at the park this weekend for a get-together. Hope you make it!'
+  },
+  {
+    sender: 'Steve Two Two',
+    receiver: 'dmmoody@gmail.com',
+    subject: 'Hello',
+    date: 'Feb 10, 2010',
+    body: 'My car is in the shop, can you give me a ride to work?'
+  },
+  {
+    sender: 'Morgan Moody',
+    receiver: 'dmmoody@gmail.com',
+    subject: 'Summer',
+    date: 'Feb 14, 2010',
+    body: 'I hope we go to the Redwoods this summer when we visit.'
+  },
+  {
+    sender: 'Great Dane Club',
+    receiver: 'dmmoody@gmail.com',
+    subject: 'Info requested',
+    date: 'Feb 28, 2010',
+    body: 'Can anyone recommend a good site about dog parks?'
+  }
+]
